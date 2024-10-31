46c4c2dabcb6830827c8345b6355c4957e66cacc
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2024-10-31 17:30:25 +0000

    update detect_platform script

--- parm/detect_platform.sh
+++ parm/detect_platform.sh
@@ -2,16 +2,20 @@
 #
 # Detect HPC platforms
 #
-if [[ -d /scratch1 ]] ; then
+if [[ -d /scratch2/NAGAPE ]] ; then
   PLATFORM="hera"
-elif [[ -d /work ]]; then
+elif [[ -d /work/noaa ]]; then
   hoststr=$(hostname)
   if [[ "$hoststr" == "hercules"* ]]; then
     PLATFORM="hercules"
   else
     PLATFORM="orion"
   fi
-elif [[ -d /jetmon ]] ; then
+elif [[ -d /ncrc ]]; then
+  PLATFORM="gaea"
+elif [[ -d /glade ]]; then
+  PLATFORM="derecho"
+elif [[ -d /lfs4/HFIP ]] ; then
   PLATFORM="jet"
 elif [[ -d /lfs/h2 ]] ; then
   PLATFORM="wcoss2"
@@ -19,3 +23,4 @@ else
   PLATFORM="unknown"
 fi
 MACHINE="${PLATFORM}"
+
